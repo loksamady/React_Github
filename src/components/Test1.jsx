51889814e3f7eff67f8f2a23330df8dc0929d203
loksamady <153054116+loksamady@users.noreply.github.com>
2025-06-26 12:56:22 +0700

    updated

---
+++ src/components/Test1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Test1 = () => {
+  return (
+    <div>
+      <h2>Hello</h2>
+      <h1>Samady</h1>
+      <button className="bg-blue-700">InCrement</button>
+    </div>
+  );
+};
