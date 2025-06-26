f178df277ab14c8e0f5e39fe76b97e518f244164
loksamady <153054116+loksamady@users.noreply.github.com>
2025-06-26 15:16:57 +0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+
 import Login from "./components/Login";
 import Test1 from "./components/Test1";
 import UserList from "./components/UserList";
--- src/components/Toogle.jsx
+++ src/components/Toogle.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import EmailIcon from "@mui/icons-material/Email";
 const App = () => {
   const [alert, setAlert] = useState(true);
   function handleToggle() {
@@ -18,7 +19,8 @@ const App = () => {
 };
 function Message() {
   return (
-    <button className="bg-red-700 w-96 py-5 text-[25px] font-bold">
+    <button className="bg-red-700 w-96 py-5 text-[25px] font-bold flex justify-center items-center gap-2">
+      <EmailIcon />
       Message...
     </button>
   );
