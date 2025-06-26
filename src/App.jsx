211c04020ff8c088b04e2143522ccc90ac826811
loksamady <153054116+loksamady@users.noreply.github.com>
2025-06-26 13:10:23 +0700

    updatad

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import "./App.css";
 import Login from "./components/Login";
+import { Test1 } from "./components/Test1";
+import UserList from "./components/UserList";
 const App = () => {
- return <>
- <Login/>
- </>
+  return (
+    <>
+      <div className="flex justify-start items-center flex-row gap-10">
+        <UserList />
+        <Test1 />
+        <Login />
+      </div>
+    </>
+  );
 };
 
 export default App;
--- src/components/Test1.jsx
+++ src/components/Test1.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Test1 = () => {
+  const [count, setCount] = useState(0);
   return (
     <div>
-      <h2>Hello</h2>
-      <h1>Samady</h1>
-      <button className="bg-blue-700">InCrement</button>
+      <h1 className="mb-4 font-bold text-[35px] text-blue-800">{count}</h1>
+      <button
+        onClick={() => setCount(count + 1)}
+        className="bg-blue-700 p-3 text-[25px] font-bold text-white"
+      >
+        Increment
+      </button>
     </div>
   );
 };
